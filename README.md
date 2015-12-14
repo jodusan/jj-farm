dd72f471513cb400b8b5937f237deb85d32a048f
dulex123 <dusanix@gmail.com>
2015-12-14 20:48:08 +0100

    GameFrame cleanup + boilerplate README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# jj-farm
-Finest flying chicken in Town!
+# README Boilerplate
+
+Change this readme as needed.
+A collection of best practices for a simple and informative README.
+
+## Installation
+
+Download to your project directory, add it, and commit.
+
+```sh
+curl -O https://github.com/christianbundy/readme-boilerplate.md
+git add README.md
+git commit -m "Add README Boilerplate"
+```
+
+## Usage
+
+Edit `README.md` with the name of your project, a description, installation instructions, usage instructions, how to get support, and how people can contribute to your project.
+
+## Support
+
+Please open an issue to receive support for this project.
+
+## Contributing
+
+Fork the project, create a new branch, make your changes, and open a pull request.
+
+We use [Clear code](http://introcs.cs.princeton.edu/java/11style/) writing guidelines.
--- engine/GameFrame.java
+++ engine/GameFrame.java
@@ -1,73 +1,37 @@
+/*---------------------------------------------------------
+ *  This class is used for visual front-end. Most important
+ *  methods are update and render, where object states are
+ *  updated and drawn on the screen for each frame. It is
+ *  extended and not used directly.
+ *---------------------------------------------------------*/
+
 package engine;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
-/**
- * Klasa namijenjena jednostavnim grafi?kim eksperimentima i demonstracijama. Koristi se
- * naslje?ivanjem i popunjavanjem apstraktnih metoda poput update() i render(), uz
- * metode koje obra?uju input doga?aje, ako je potrebna interakcija.
- * @author Aleksandar Stan?i?
- *
- */
-public abstract class GameFrame extends JPanel implements MouseListener, 
-	MouseWheelListener, MouseMotionListener, KeyListener
+public abstract class GameFrame extends JPanel
 {
 	private static final long serialVersionUID = 6058915663486070170L;
-
-	public static enum GFMouseButton
-	{
-		None,
-		Left,
-		Middle,
-		Right,
-		WheelUp,
-		WheelDown,
-		WheelLeft,
-		WheelRight,
-		Special1,
-		Special2
-	}
 
 	private int screenX = 640;
 	private int screenY = 480;
 	
 	private static JFrame myFrame = null;
-	private String title = "RAF GameFrame";
+	private String title = "";
 	
 	private Color backColor = Color.black;
 	private boolean clearBackBuffer = true;
-	
-	private int mouseX = 0;
-	private int mouseY = 0;
-	
+
 	private int updateRate = 30;
 	private boolean updatesRunning = true;
 	private boolean renderRunning = true;
-	
-	private boolean useHQ = false;
-	
+
 	private Thread runnerThread = null;
-	
-	private boolean[] mouseButtons = new boolean[GFMouseButton.values().length];
-	private boolean[] keyboardKeys = new boolean[1024];
-	
-	/**
-	 * Konstruktor za GameFrame, koji se mora pozvati iz naslije?enih klasa
-	 * @param title naslov prozora
-	 * @param sizeX ?irina u pikselima
-	 * @param sizeY visina u pikselima
-	 */
+
 	public GameFrame(String title, int sizeX, int sizeY)
 	{
 		super(true);
-		
-		if(sizeX < 320) sizeX = 320;
-		if(sizeY < 240) sizeY = 240;
-		
-		if(sizeX > 2048) sizeX = 2048;
-		if(sizeY > 2048) sizeY = 2048;
 		
 		screenX = sizeX;
 		screenY = sizeY;
@@ -77,43 +41,33 @@ public abstract class GameFrame extends JPanel implements MouseListener,
 		if(title != null) this.title = title;
 		
 		this.title = title;
-		
-		addMouseListener(this);
-		addMouseMotionListener(this);
-		addMouseWheelListener(this);
-		addKeyListener(this);
-		
-		runnerThread = new Thread(new Runnable()
-		{
-			
-			@Override
-			public void run()
-			{
-				while(true)
-				{
-					long startTime = System.currentTimeMillis();
-					if(updatesRunning) tick();
-					if(renderRunning) repaint();
-					try
-					{
-						long frameTime = System.currentTimeMillis() - startTime;
-						long sleepTime = 1000 / updateRate - frameTime;
-						if(sleepTime > 0)
-							Thread.sleep(sleepTime);
-					} 
-					catch (InterruptedException e) { }
-				}
-			}
-		});
-		
-		//runnerThread.start();
+
+        runnerThread = new Thread(() -> {
+            while(true)
+            {
+                long startTime = System.currentTimeMillis();
+                if(updatesRunning) tick();
+                if(renderRunning) repaint();
+                try
+                {
+                    long frameTime = System.currentTimeMillis() - startTime;
+                    long sleepTime = 1000 / updateRate - frameTime;
+                    if(sleepTime > 0)
+                        Thread.sleep(sleepTime);
+                }
+                catch (InterruptedException e) { }
+            }
+        });
+
+        //runnerThread.start();
 	}
-	
-	/**
-	 * Po?etak rada glavnog threada koji poziva update() i render() metoda, mora
-	 * se pozvati kako bi aplikacija po?ela sa radom, najbolje na kraju naslije?enog
-	 * konstruktora, nakon ?to se svi resursi u?itaju i pripreme.
-	 */
+
+    /*---------------------------------------------------------
+     *  Main thread which calls update() and render() methods.
+     *  It needs to be called for application to start.
+     *  The best place for it to be called is at the end of
+     *  constructor, after all resources are loaded.
+     *---------------------------------------------------------*/
 	public void startThread()
 	{
 		if(!runnerThread.isAlive())
@@ -126,11 +80,12 @@ public abstract class GameFrame extends JPanel implements MouseListener,
 	{
 		update();
 	}
-	
-	/**
-	 * Inicijalizacija prozora (JFrame) u kome se nalazi panel igre, potrebno pozvati
-	 * nakon zavr?etka konstruktora.
-	 */
+
+
+    /*---------------------------------------------------------
+     *  Initialise JFrame with main panel. Needs to be called
+     *  at the end of constructor.
+     *---------------------------------------------------------*/
 	public void initGameWindow()
 	{
 		if(myFrame != null)
@@ -149,8 +104,7 @@ public abstract class GameFrame extends JPanel implements MouseListener,
 		myFrame.pack();
 		myFrame.setVisible(true);
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
-		myFrame.addKeyListener(this);
+
 		
 		myFrame.setIgnoreRepaint(true);
 		
@@ -172,218 +126,47 @@ public abstract class GameFrame extends JPanel implements MouseListener,
 			g.setColor(backColor);
 			g.fillRect(0, 0, screenX, screenY);
 		}
-		
-		if(useHQ) 
-		{
-			((Graphics2D)g).setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-			((Graphics2D)g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		}
-		else
-		{
-			((Graphics2D)g).setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_SPEED);
-			((Graphics2D)g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
-		}
+
+		((Graphics2D)g).setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		((Graphics2D)g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		
 		render((Graphics2D)g, getWidth(), getHeight());
 	}
-
-	@Override
-	public void mouseClicked(MouseEvent arg0) {}
-
-	@Override
-	public void mouseEntered(MouseEvent arg0) {}
-
-	@Override
-	public void mouseExited(MouseEvent arg0) {}
-
-	@Override
-	public void mousePressed(MouseEvent arg0)
-	{
-		GFMouseButton button = GFMouseButton.None;
-		if(arg0.getButton() == 1) button = GFMouseButton.Left;
-		if(arg0.getButton() == 2) button = GFMouseButton.Middle;
-		if(arg0.getButton() == 3) button = GFMouseButton.Right;
-		
-		if(arg0.getButton() == 5) button = GFMouseButton.Special1;
-		if(arg0.getButton() == 4) button = GFMouseButton.Special2;
-		
-		mouseX = arg0.getX();
-		mouseY = arg0.getY();
-		
-		mouseButtons[button.ordinal()] = true;
-		handleMouseDown(mouseX, mouseY, button);	
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0)
-	{
-		GFMouseButton button = GFMouseButton.None;
-		if(arg0.getButton() == 1) button = GFMouseButton.Left;
-		if(arg0.getButton() == 2) button = GFMouseButton.Middle;
-		if(arg0.getButton() == 3) button = GFMouseButton.Right;
-		
-		if(arg0.getButton() == 5) button = GFMouseButton.Special1;
-		if(arg0.getButton() == 4) button = GFMouseButton.Special2;
-		
-		mouseX = arg0.getX();
-		mouseY = arg0.getY();
-		
-		mouseButtons[button.ordinal()] = false;
-		handleMouseUp(mouseX, mouseY, button);	
-	}
-
-	@Override
-	public void keyPressed(KeyEvent arg0)
-	{
-		if(arg0.getKeyCode() >= 0 && arg0.getKeyCode() < 1024)
-			keyboardKeys[arg0.getKeyCode()] = true;
-		handleKeyDown(arg0.getKeyCode());
-	}
-
-	@Override
-	public void keyReleased(KeyEvent arg0)
-	{
-		if(arg0.getKeyCode() >= 0 && arg0.getKeyCode() < 1024)
-			keyboardKeys[arg0.getKeyCode()] = false;
-		handleKeyUp(arg0.getKeyCode());
-	}
-
-	@Override
-	public void keyTyped(KeyEvent arg0) {}
-
-	@Override
-	public void mouseDragged(MouseEvent arg0) 
-	{
-		mouseX = arg0.getX();
-		mouseY = arg0.getY();
-		
-		handleMouseMove(mouseX, mouseY);
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent arg0)
-	{
-		mouseX = arg0.getX();
-		mouseY = arg0.getY();
-		
-		handleMouseMove(mouseX, mouseY);
-	}
-
-	@Override
-	public void mouseWheelMoved(MouseWheelEvent arg0)
-	{
-		if(arg0.getWheelRotation() > 0)
-		{
-			mouseButtons[GFMouseButton.WheelDown.ordinal()] = true;
-			mouseX = arg0.getX();
-			mouseY = arg0.getY();
-			handleMouseDown(mouseX, mouseY, GFMouseButton.WheelDown);
-			mouseButtons[GFMouseButton.WheelDown.ordinal()] = false;
-		}
-		else if(arg0.getWheelRotation() < 0)
-		{
-			mouseButtons[GFMouseButton.WheelUp.ordinal()] = true;
-			mouseX = arg0.getX();
-			mouseY = arg0.getY();
-			handleMouseDown(mouseX, mouseY, GFMouseButton.WheelUp);
-			mouseButtons[GFMouseButton.WheelUp.ordinal()] = false;
-		}
-	}
 	
-	/**
-	 * Daje trenutni status tipki mi?a 
-	 * @param button dugme iz GFMouseButton enuma za koje se tra?i status
-	 * @return true ako je pritisnuta, false ako nije
-	 */
-	protected boolean isMouseButtonDown(GFMouseButton button)
-	{
-		return mouseButtons[button.ordinal()];
-	}
-	
-	/**
-	 * Daje trenutni status tipki tastature 
-	 * @param keyCode konstanta iz KeyEvent koja odre?uje tipku
-	 * @return true ako je pritisnuta, false ako nije
-	 */
-	protected boolean isKeyDown(int keyCode)
-	{
-		if(keyCode >= 0 && keyCode < 1024)
-			return keyboardKeys[keyCode];
-		else
-			return false;
-	}
-	
-	/**
-	 * Daje JFrame objekat trenutnog okvira
-	 * @return JFrame igre (konstruisan prilikom initGameWindow poziva)
-	 */
 	protected JFrame getWindow()
 	{
 		return myFrame;
 	}
-	
-	/**
-	 * Pali ili gase automatsko brisanje pozadine prije render() metode
-	 * @param clr da li treba raditi clear
-	 */
+
 	protected void setBackgroundClear(boolean clr)
 	{
 		clearBackBuffer = clr;
 	}
-	
-	/**
-	 * Postavlja boju na koju ?e pozadina biti postavljena, ako je uklju?en setBackgroundClear
-	 * @param c boja
-	 */
+
+
+    /*---------------------------------------------------------
+     *  Sets backround color
+     *---------------------------------------------------------*/
 	protected void setBackgroundClearColor(Color c)
 	{
 		backColor = c;
 	}
-	
-	/**
-	 * Trenutna X koordinata mi?a, u prostoru panela za crtanje
-	 * @return X koordinata
-	 */
-	protected int getMouseX()
-	{
-		return mouseX;
-	}
-	
-	/**
-	 * Trenutna Y koordinata mi?a, u prostoru panela za crtanje
-	 * @return Y koordinata
-	 */
-	protected int getMouseY()
-	{
-		return mouseY;
-	}
-	
-	/**
-	 * Postavlja hint za vi?i kvalitet iscrtavanja koji ?e se onda automatski primjenjivati
-	 * nad Graphics2D objektom koji se daje u render() metodi
-	 * @param hq true za vi?i kvalitet interpolacije i uklju?en anti-aliasing primitiva
-	 */
-	protected void setHighQuality(boolean hq)
-	{
-		useHQ = hq;
-	}
-	
-	/**
-	 * Postavlja ikonicu prozora
-	 * @param icon Image objekat (mo?e biti BufferedImage)
-	 */
+
+
+    /*---------------------------------------------------------
+     *  Main window icon
+     *---------------------------------------------------------*/
 	protected void setIcon(Image icon)
 	{
 		myFrame.setIconImage(icon);
 	}
-	
-	/**
-	 * Postavlja ciljnu frekvenciju a?uriranja u Hz/fps. Tajming je realizovan jednostavnim
-	 * sleep metodama, zbog ?ega je mogu? neravnomijeran tok izvr?avanja (judder). Ukoliko
-	 * a?uriranje i iscrtavanje traje du?e od (1 / fps) sekundi, svukupan tempo izvr?avanja
-	 * ?e se usporiti na tu brzinu, nije implementiran nikakav dinami?ki update ili frameskipping.  
-	 * @param fps ciljni broj a?uriranih i iscrtanih okvira u sekundi, od 1 do 120
-	 */
+
+
+    /*---------------------------------------------------------
+     *  Sets refresh rate ( Hz/fps )
+     *  Timing is made with simple sleep methods which can
+     *  cause judder. No dynamic update or frameskipping.
+     *---------------------------------------------------------*/
 	protected void setUpdateRate(int fps)
 	{
 		if(fps >= 1 && fps < 120)
@@ -395,63 +178,31 @@ public abstract class GameFrame extends JPanel implements MouseListener,
 			System.out.println("Valid range for setUpdateRate is 1 - 120");
 		}
 	}
-	
-	/**
-	 * Metod ?e biti pozvan prilikom initGameWindow() poziva, nakon ?to je prozor konstruisan.
-	 */
+
+
+    /*---------------------------------------------------------
+     *  Fires within initGameWindow() call after
+     *  the window is constructed.
+     *---------------------------------------------------------*/
 	public abstract void handleWindowInit();
-	
-	/**
-	 * Poziva se prilikom ga?enja prozora (ako je korisnik kliknuo na X)
-	 */
+
+
+    /*---------------------------------------------------------
+     *  Fires when user quits the application.
+     *  (Presses the X on the window)
+     *---------------------------------------------------------*/
 	public abstract void handleWindowDestroy();
-	
-	/**
-	 * Metod koji treba da obavi kompletno iscrtavanje cijelog frejma, poziva se automatski,
-	 * zadatom frekvencijom (update rate). Ne treba da sadr?i nikakva logi?ka a?uriranja, samo crtanje.
-	 * @param g Graphics2D objekat preko koga se obavlja crtanje na ekran
-	 * @param sw ?irina trenutnog prostora za crtanje
-	 * @param sh visina trenutnog prostora za crtanje
-	 */
+
+
+    /*---------------------------------------------------------
+     *  Draws objects on the window. No updates!
+     *---------------------------------------------------------*/
 	public abstract void render(Graphics2D g, int sw, int sh);
-	
-	/**
-	 * Metod koji treba da a?urira stanje igre, poziva se prije render() poziva, jednakom frekvencijom.
-	 */
+
+
+    /*---------------------------------------------------------
+     *  Updates the states of objects. No drawing!
+     *---------------------------------------------------------*/
 	public abstract void update();
-	
-	/**
-	 * Metod koji ?e biti pozvan na pritisak tastera mi?a (okretanja scroll to?ka se tako?e smatraju tasterima) 
-	 * @param x X koordinata u pikselima na kojima je kursor bio u trenutku pritiska
-	 * @param y Y koordinata u pikselima na kojima je kursor bio u trenutku pritiska
-	 * @param button dugme mi?a koje je pritisnuto, iz GFMouseButton enuma
-	 */
-	public abstract void handleMouseDown(int x, int y, GFMouseButton button);
-	
-	/**
-	 * Metod koji ?e biti pozvan na pu?tanje tastera mi?a (okretanja scroll to?ka se tako?e smatraju tasterima) 
-	 * @param x X koordinata u pikselima na kojima je kursor bio u trenutku pu?tanja
-	 * @param y Y koordinata u pikselima na kojima je kursor bio u trenutku pu?tanja
-	 * @param button dugme mi?a koje je pu?teno, iz GFMouseButton enuma
-	 */
-	public abstract void handleMouseUp(int x, int y, GFMouseButton button);
-	
-	/**
-	 * Metod koji se poziva pri svakoj promjeni pozicije kursora mi?a, bez obzira da li su tasteri pritisnuti.
-	 * @param x X koordinata kursora u pikselima
-	 * @param y Y koordinata kursora u pikselima
-	 */
-	public abstract void handleMouseMove(int x, int y);
-	
-	/**
-	 * Metod koji se poziva kada je pritisnut taster na tastaturi.
-	 * @param keyCode kod tipke koja je pritisnuta, porediti sa vrijednostima iz KeyEvent.VK_*
-	 */
-	public abstract void handleKeyDown(int keyCode);
-	
-	/**
-	 * Metod koji se poziva kada je pu?teni taster na tastaturi.
-	 * @param keyCode kod tipke koja je pu?tena, porediti sa vrijednostima iz KeyEvent.VK_*
-	 */
-	public abstract void handleKeyUp(int keyCode);
+
 }
